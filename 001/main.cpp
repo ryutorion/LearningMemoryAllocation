35bf81857859850b43a205bbc693d37afd6d5809
Masahiro Kimoto <masahiro.kimoto@gmail.com>
2019-06-28 22:50:36 +0900

    メモリの確保と解放とそのログを出力するだけのサンプルを実装

---
+++ 001/main.cpp
@@ -0,0 +1,30 @@
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+void * my_alloc(size_t size)
+{
+	void * p = VirtualAlloc(nullptr, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+	cerr << "Allocate " << size << " at " << p << endl;
+
+	return p;
+}
+
+void my_free(void * p)
+{
+	VirtualFree(p, 0, MEM_RELEASE);
+
+	cerr << "Deallocate " << p << endl;
+}
+
+int main()
+{
+	size_t size = 1;
+	for(int i = 0; i < 32; ++i, size <<= 1)
+	{
+		void * p = my_alloc(size);
+		my_free(p);
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LearningMemoryAllocation
 メモリアロケーションの学習
+
+## 001
+
+VirtualAlloc，VirtualFreeで指定したサイズのメモリの確保と解放を行い，そのログを出力するだけのサンプル．
